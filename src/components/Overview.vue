ed635c04cde8709bcf13b53f48fa34c25b298aa1
Safaridin Salim <safaridin.salim@gmail.com>
2017-12-27 14:53:50 +0700

    Final results

---
+++ src/components/Overview.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="overview">
+    <day-select :selected="day"></day-select>  
+    <div class="main">
+      <movie-list :genre="genre" :time="time" :movies="movies" :day="day"></movie-list>
+      <movie-filter></movie-filter>
+    </div>
+  </div>
+</template>
+
+<script>
+  import MovieList from './MovieList.vue';
+  import MovieFilter from './MovieFilter.vue';
+  import DaySelect from './DaySelect.vue';
+
+  export default {
+    props: ['genre', 'time', 'movies', 'day'],
+    components: {
+      MovieList, MovieFilter, DaySelect
+    }
+  }
+</script>
